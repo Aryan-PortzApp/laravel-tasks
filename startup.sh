cba11b376bab4f9ee3df11630627caa311b9a604
Aryan Prince <aryanprince@portzapp.com>
2025-08-29 15:04:06 +0530

    feat: Setup Node and build frontend assets

--- startup.sh
+++ startup.sh
@@ -1,2 +1,15 @@
+# 1. Copy nginx configuration
 cp /home/site/wwwroot/default /etc/nginx/sites-available/default && service nginx restart
+
+# 2. Run new DB migration files
 php artisan migrate --force
+
+#3. Build frontend assets
+#3.1 Install Node.js
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 22
+
+#3.2 Install and build Node.js dependencies
+npm install
+npm run build
